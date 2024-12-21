3d35b6cf17cadd3e663b70bf792b7b8061a5bee6
Md Emdadul Haque <MEMHA@orsted.com>
2024-12-21 08:12:05 +0800

    add PlusOneArray problem

---
+++ DSA.ArrayProblems/PlusOneArray.cs
@@ -0,0 +1,65 @@
+﻿using System.Numerics;
+
+namespace DSA.ArrayProblems;
+
+public  class PlusOneArray
+{
+    public int[] GetPlusOne(int[] digits)
+    {
+        
+
+        BigInteger number = new();
+
+        foreach (var digit in digits)
+        {
+            number = number * 10 + digit; // Shift left and add the current digit
+        }
+
+        BigInteger resultNumber = number + 1;
+        //BigInteger countResultNumber = resultNumber;
+
+        //int count = 0;
+        //while (countResultNumber > 0)
+        //{
+        //    countResultNumber /= 10;
+        //    count++;
+        //}
+
+
+        //int[] results = new int[count];
+        //int index = results.Length - 1;
+        //while (resultNumber > 0)
+        //{
+        //    results[index] = (int)(resultNumber % 10);
+        //    resultNumber /= 10;
+        //    index--;
+        //}
+
+        //Convert the resulting BigInteger back into an array of digits
+        string resultString = resultNumber.ToString();
+        int[] results = new int[resultString.Length];
+        for (int i = 0; i < resultString.Length; i++)
+        {
+            results[i] = resultString[i] - '0'; // Convert char to int
+        }
+
+        return results;
+
+    }
+
+    public BigInteger GettheSameArray(int[] digits)
+    {
+
+
+        BigInteger number = 0;
+
+        foreach (var digit in digits)
+        {
+            number = number * 10 + digit; // Shift left and add the current digit
+        }
+
+        var a = number.ToByteArray();
+
+        return number + 1;
+    }
+}
---
+++ DSA.UnitTest/PlusOneArrayTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Numerics;
+using DSA.ArrayProblems;
+using FluentAssertions;
+
+namespace DSA.UnitTest
+{
+    public class PlusOneArrayTests
+    {
+        [Fact]
+        public void GetPlusOne_ShouldHandleSingleDigitCorrectly()
+        {
+            //Arrange
+            var sut = new PlusOneArray();
+            int[] nums = [9];
+
+
+            //Act
+            var result = sut.GetPlusOne(nums);
+
+            //Assert
+            result.Should().Equal(1, 0);
+        }
+
+        [Fact]
+        public void GetPlusOne_ShouldHandleMultipleDigitsCorrectly()
+        {
+            //Arrange
+            var sut = new PlusOneArray();
+            int[] nums = [1, 2, 3];
+
+
+            //Act
+            var result = sut.GetPlusOne(nums);
+
+            //Assert
+            result.Should().Equal(1, 2, 4);
+        }
+
+        [Fact]
+        public void GetPlusOne_ShouldHandleMultipleDigitsCorrectly1()
+        {
+            //Arrange
+            var sut = new PlusOneArray();
+            int[] nums = [9, 9];
+
+
+            //Act
+            var result = sut.GetPlusOne(nums);
+
+            //Assert
+            result.Should().Equal(1, 0, 0);
+        }
+
+        [Fact]
+        public void GetPlusOne_ShouldHandleMultipleDigitsCorrectly2()
+        {
+            //Arrange
+            var sut = new PlusOneArray();
+            int[] nums = [7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6];
+
+
+            //Act
+            var result = sut.GetPlusOne(nums);
+
+            //Assert
+            result.Should().Equal(7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 7);
+        }
+
+        [Fact]
+        public void GettheSameArray_ShouldReturnTheSameArray()
+        {
+            //Arrange
+            var sut = new PlusOneArray();
+            int[] nums = [7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6];
+
+
+            //Act
+            var result = sut.GettheSameArray(nums);
+
+            BigInteger expectedResult = BigInteger.Parse("728509129536673284379577474947011174007");
+            //Assert
+            result.Should().Be(expectedResult);
+        }
+    
+    }
+}
